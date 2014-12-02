cc8a52947da66ca708a7bd74c91ff4f79738ac65
kgentner <gentnerkarl@gmail.com>
2014-12-01 17:26:58 -0800

    renamed tests

--- test/client/frontend_test.js
+++ test/client/frontend_test.js
@@ -4,11 +4,11 @@ var isClicked = require('../../app/js/isClicked');
 var isEntered = require('../../app/js/isEntered');
 
 describe('Very Simple Client-Side JS Testing', function() {
-  it('should return true when clicked', function() {
+  it('should return true when isClicked function is called', function() {
     expect(isClicked()).to.eql(true);
   });
 
-  it('should return true when entered', function() {
+  it('should return true when isEntered function is called', function() {
     expect(isEntered()).to.eql(true);
   });
 });
